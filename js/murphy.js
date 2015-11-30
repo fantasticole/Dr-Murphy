5c04efa3dad7501b7dbc23a66fb88a88a62a3fe1
FantastiCole <coleamurphy@gmail.com>
2015-11-30 13:32:55 -0500

    Overhaul previous styling and start (basically) from scratch.

--- js/murphy.js
+++ js/murphy.js
@@ -1,30 +1,31 @@
 function main(){
 	var here = window.location.href;
 	
-	if (here.indexOf('#') > -1){
-		var id = here.slice(here.indexOf('#'));
-		$('.active').removeClass('active');
-		$('.here').removeClass('here');
-		$(id).addClass('active');
-		var navItems = $('li a');
-		for (var x = 0; x < navItems.length; x++){
-			if (navItems[x].hash === id){
-				navItems[x].setAttribute('class', 'here');
-			}
-		}
-	};
+	// if (here.indexOf('#') > -1){
+	// 	var id = here.slice(here.indexOf('#'));
+	// 	$('.active').removeClass('active');
+	// 	$('.here').removeClass('here');
+	// 	$(id).addClass('active');
+	// 	var navItems = $('li a');
+	// 	for (var x = 0; x < navItems.length; x++){
+	// 		if (navItems[x].hash === id){
+	// 			navItems[x].setAttribute('class', 'here');
+	// 		}
+	// 	}
+	// };
 
 	$('li a').click(function(){
-		var id = this.attributes[0].nodeValue;
+		var id = this.className;
 		$('.active').removeClass('active');
 		$('.here').removeClass('here');
-		$(id).addClass('active');
+		$('#'+id).addClass('active');
 		$(this).addClass('here');
+		$('.navbar-collapse').removeClass('in');
 	});
-	
-	$('.moreInfo').click(function(){
-		$('#extended').css({'display':'block'});
-	});
+
+	// $('.moreInfo').click(function(){
+	// 	$('#extended').css({'display':'block'});
+	// });
 
 	$('#extended').click(function(){
 		$(this).css({'display':'none'});
